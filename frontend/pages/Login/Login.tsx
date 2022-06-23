3debd160b0d8e1d97cae61bc017428a08f7564c1
Piter Merlo Bruner <merlobruner@gmail.com>
2022-06-23 11:45:06 -0300

    Executando um novo build ja com React Router

--- frontend/pages/Login/Login.tsx
+++
@@ -1,8 +0,0 @@
-export function Login() {
-    return (
-      <div>
-        <h1>Ola Login</h1>
-      </div>
-    );
-  }
-  
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../pages/Home/Home";
-import { Login } from "../pages/Login/Login";
+import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
 
 export function App() {
   return (
