154cf1d9f339826a86a06aef726e5e99b17ef6d1
Piter Merlo Bruner <merlobruner@gmail.com>
2022-06-24 14:33:16 -0300

    feature: Adicionando Auth JWT para chamada API

--- backend/AppPeoplesList/views.py
+++ backend/AppPeoplesList/views.py
@@ -1,9 +1,11 @@
+from re import I
 from rest_framework import generics, permissions
 from .models import Person
 from .serializers import PersonSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class PersonListAPIView(generics.ListAPIView):
     serializer_class = PersonSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = (IsAuthenticated, )
     queryset = Person.objects.all()
--- backend/peoplesList/urls.py
+++ backend/peoplesList/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('AppPeoplesList.urls')),
 ]
